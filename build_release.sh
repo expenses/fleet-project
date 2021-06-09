771ec2681c614b4496131974e194091cfe92bbba
Ashley Ruglys <ashley.ruglys@gmail.com>
2021-06-10 00:40:52 +0200

    Build release script puts files in a dir

--- build_release.sh
+++ build_release.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
+mkdir -p release
 ~/.cargo/bin/cross build --release --target x86_64-unknown-linux-gnu
 ~/.cargo/bin/cross build --release --target x86_64-pc-windows-gnu
-mv target/x86_64-unknown-linux-gnu/release/fleet-renderer .
-mv target/x86_64-pc-windows-gnu/release/fleet-renderer.exe .
+mv target/x86_64-unknown-linux-gnu/release/fleet-renderer release
+mv target/x86_64-pc-windows-gnu/release/fleet-renderer.exe release
