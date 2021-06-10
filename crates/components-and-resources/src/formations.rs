72be5c93e6dc43e5118355bfc1fd24e912c7e9c1
Ashley Ruglys <ashley.ruglys@gmail.com>
2021-06-10 14:01:22 +0200

    Add a sphere formation

--- crates/components-and-resources/src/formations.rs
+++ crates/components-and-resources/src/formations.rs
@@ -1,3 +1,4 @@
+use rand::{rngs::SmallRng, SeedableRng};
 use ultraviolet::Vec3;
 
 #[derive(Debug)]
@@ -56,6 +57,26 @@ impl Formation {
                     let y = (i / width) as f32 - middle_y;
 
                     FormationPosition::new(position + sideways * x + up * y)
+                })
+                .collect(),
+        }
+    }
+
+    pub fn in_sphere(position: Vec3, count: usize) -> Self {
+        // Volume of a sphere: 4/3 * pi * r ^ 3
+        // Radius from volume: v ^ 1/3 / pi * 3/4
+
+        let radius = (count as f32).powf(1.0 / 3.0) / std::f32::consts::PI * 3.0 / 4.0;
+        let radius = radius * 20.0;
+
+        let mut rng = SmallRng::seed_from_u64(0);
+
+        Self {
+            positions: (0..count)
+                .map(|_| {
+                    FormationPosition::new(
+                        position + crate::utils::random_point_in_sphere(&mut rng) * radius,
+                    )
                 })
                 .collect(),
         }
--- crates/components-and-resources/src/utils.rs
+++ crates/components-and-resources/src/utils.rs
@@ -25,3 +25,11 @@ pub fn rotation_from_facing(facing: Vec3) -> Rotor3 {
     Rotor3::from_rotation_xz(-facing.x.atan2(facing.z))
         * Rotor3::from_rotation_yz(-facing.y.atan2(xz_movement))
 }
+
+// https://datagenetics.com/blog/january32020/index.html
+pub fn random_point_in_sphere<R: Rng>(rng: &mut R) -> Vec3 {
+    let on_sphere = uniform_sphere_distribution(rng);
+    let distance_from_center = rng.gen_range(0.0_f32..1.0).powf(1.0 / 3.0);
+
+    on_sphere * distance_from_center
+}
